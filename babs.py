4ca459657cd43f89e385111a4504cdaaf1b99283
bterweeme <b.ter.weeme@student.vu.nl>
2025-04-21 15:56:56 +0200

    Update babs.py

--- babs.py
+++ babs.py
@@ -2,7 +2,6 @@ import argparse
 from InstanceCO25 import InstanceCO22
 
 from collections import defaultdict 
-import numpy as np
 
 
 
@@ -17,8 +16,21 @@ def ReadInstance():
     if not instance.isValid():
         print("Invalid instance.")
         return
-    
     return instance
+
+def formatRequest(instance): 
+        # returns the request in a format that is easier to work with
+        formatted = []
+
+        for request in instance.Requests:
+            ID_request = request.ID
+            day = request.desiredDay
+            locationID_request = request.customerLocID
+            amounts = request.amounts
+
+            formatted.append([ID_request, day, locationID_request, amounts])
+
+        return formatted
 
 
 def Optimize(instance):
@@ -43,13 +55,34 @@ def Optimize(instance):
             result.append([hubID, day, locationID_hub, amounts])
         
         return result # hubID day location amount
+     
+class Vehicle:
+    def __init__(self, vehicle_type, vehicle_id, capacity, milage, visits, products):
+        self.vehicle_type = vehicle_type
+        self.vehicle_id = vehicle_id
+        self.capacity = capacity
+        self.milage = milage
+        self.visits = visits
+        self.products = products
+        #self.list_of_products = defaultdict(int)  # Default dictionary to store product quantities
+
+
+    def load(self, amount, distance):
+        total_amount = np.sum(amount)
+        self.products += amount
+        self.capacity -= total_amount
+        self.milage -= distance
+    
+    def return_carried_products(self):
+        return self.products
+    
         
 def WriteResults():
     print("Writing results...")
 
 if __name__ == "__main__":
     instance = ReadInstance()
-    print(instance.Locations)
+    print(instance.Requests[0].customerLocID)
     Optimize(instance)
     WriteResults()
     print("Done.")
